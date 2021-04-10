354ab60fc2952a758d3be8c6b8b8edfd0740493e
rgoshen <rick.goshen@gmail.com>
2021-04-10 16:39:08 -0700

    finish helpers tests

--- helpers.test.js
+++ helpers.test.js
@@ -1,11 +1,25 @@
-// put tests in here and rename file if needed
-const { calculateMean } = require("./helpers");
+const { findMean, findMedian, findMode } = require("./helpers");
 
-describe("median function", function () {
-  test("calculates the median of an even set", function () {
-    expect(calculateMean([10, 5, -5, 2])).toEqual(3);
+describe("#findMedian", function () {
+  it("finds the median of an even set", function () {
+    expect(findMedian([1, -1, 4, 2])).toEqual(1.5);
   });
-  test("calculates the median of an odd set", function () {
-    expect(calculateMean([10, -6, 2])).toEqual(2);
+  it("finds the median of an odd set", function () {
+    expect(findMedian([1, -1, 4])).toEqual(1);
+  });
+});
+
+describe("#findMean", function () {
+  it("finds the mean of an empty array", function () {
+    expect(findMean([])).toEqual(0);
+  });
+  it("finds the mean of an array of numbers", function () {
+    expect(findMean([1, -1, 4, 2])).toEqual(1.5);
+  });
+});
+
+describe("#findMode", function () {
+  it("finds the mode", function () {
+    expect(findMode([1, 1, 1, 2, 2, 3])).toEqual(1);
   });
 });
