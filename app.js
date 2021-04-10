6ee6176480bde60ed8d198a4219e2803ea74f287
rgoshen <rick.goshen@gmail.com>
2021-04-10 15:47:52 -0700

    add general error handlers

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ app.get("/mean", function (req, res) {
   return res.send(result);
 });
 
+/** general error handler */
+
+app.use(function (req, res, next) {
+  const err = new ExpressError("Not Found", 404);
+
+  // pass the error to the next piece of middleware
+  return next(err);
+});
+
+/** general error handler */
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message,
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
